09414e6cc13385993d88f233d5e02a57adf6c798
nikhilsaitejavanam <nikhilsaitejavanam@gmail.com>
2026-09-22 12:34:53 +0530

    playground specs removed from tracking

--- tests/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-import { logger } from "../helpers/logger";
-
-test("Sample test", async ({ page }) => {
-  logger.info("Sample test started");
-  logger.warn("Sample test warning");
-  logger.error("Sample test error");
-});
